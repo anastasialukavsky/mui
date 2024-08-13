dcc9a4185fdf6579a20a0c2430ce2cc6eccc726c
Anastasia Lukavsky <92340133+anastasialukavsky@users.noreply.github.com>
2024-08-12 20:58:08 -0400

    Add new component barrel exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { Navbar } from './Navbar';
 export { Search } from './Search';
 export { PokemonList } from './PokemonList';
 export { PokemonCard } from './PokemonCard';
+export { AddRemoveBtn } from './Button';
+export { MySquad } from './MySquad';
